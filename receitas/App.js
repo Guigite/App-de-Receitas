0cdcd5019d8f0a3fab7ec62a07ef62acf2ec1178
Guigite <guicunhaalves@gmail.com>
2023-02-06 18:01:03 -0300

    commit 02

--- receitas/App.js
+++ receitas/App.js
@@ -1,13 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
+  
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState();
+
+  useEffect (() => {
+
+    async function PegarDados() {
+      await fetch ("http://localhost:8080/receitas")
+      .then((resp) => {return resp.json()})
+      .then((dados) => {
+        setData(dados); setLoading(false)
+      })
+      .catch((err) => console.log(err))
+    }
+
+    PegarDados();
+
+  });
+  
+  if (loading){
+   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Buscando dados...</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  ); 
+  }else{
+    return (
+      <View style={styles.container}>
+        <Text>{data[0].nome}</Text>
+        <StatusBar style="auto" />
+      </View>
+    ); 
+  }
+  
 }
 
 const styles = StyleSheet.create({
